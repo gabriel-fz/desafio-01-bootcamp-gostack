fd802fc10ee4e79f3e847a00329ac481c7a9c148
gabriel-fz <gabrielfzancanela@gmail.com>
2020-02-13 00:30:59 -0300

    rotas implementadas

--- index.js
+++ index.js
@@ -4,8 +4,61 @@ const server = express();
 
 server.use(express.json());
 
-server.get("/teste", (req, res) => {
-  return res.json({ message: "Hello World" });
+const projects = [];
+
+// Cadastra um novo projeto
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(projects);
+});
+
+// Lista todos os projetos
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+// Edita o títilo do projeto
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(pjt => pjt.id == id);
+
+  project.title = title;
+
+  return res.json(projects);
+});
+
+// Deleta o projeto
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = projects.findIndex(pjt => pjt.id == id);
+
+  projects.splice(index, 1);
+
+  return res.json(projects);
+});
+
+// Armazena tarefas
+server.post("/projects/:id/tasks", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(pjt => pjt.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(projects);
 });
 
 server.listen(3000);
